4b339dd4e4bef7346acb6900df031d57b16659cd
Ariel429 <arielkim429@gmail.com>
2020-10-02 16:31:47 +0900

    implementation of input loop and close

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -10,7 +10,7 @@ public class Calendar {
 	}
 	
 	public static void printSampleCalendar() {
-		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("SU MO TU WE TH FR SA");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
@@ -21,18 +21,21 @@ public class Calendar {
 	public static void main(String[] args) {
 		// printSampleCalendar();
 		Scanner scanner= new Scanner(System.in);
+		Calendar calendar = new Calendar();
 		System.out.println("반복할 횟수를 입력하세요: ");
 		int iterationNum= scanner.nextInt();
 		
-		Calendar calendar = new Calendar();
 		while(iterationNum > 0) {
 			iterationNum--;
 			System.out.println("월을 입력하세요: ");
 			int month= scanner.nextInt();
-			System.out.printf("%d월은 %d일까지 있습니다. \n", month, calendar.getMdaysOfMonth(month));
+			if(month <= 0) {
+				System.out.println("Have a nice day!");
+			}else {
+				System.out.printf("%d월은 %d일까지 있습니다. \n", month, calendar.getMdaysOfMonth(month));
+			}
 		}
-		System.out.println("반복을 종료합니다.");
+		System.out.println("Calendar를 종료합니다.");
 		scanner.close();
 	}
 }
-
