77c7fb3cb2e550895cb6b0fdc585ffdb69fe6e34
Ariel429 <arielkim429@gmail.com>
2020-10-02 15:01:18 +0900

    update to able input loop

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -2,13 +2,37 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	public static void main(String[] args) {
+	
+	private final int [] MAX_DAYS= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMdaysOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
+	
+	public static void printSampleCalendar() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-			
+	}
+	
+	public static void main(String[] args) {
+		// printSampleCalendar();
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("반복할 횟수를 입력하세요: ");
+		int iterationNum= scanner.nextInt();
+		
+		Calendar calendar = new Calendar();
+		while(iterationNum > 0) {
+			iterationNum--;
+			System.out.println("월을 입력하세요: ");
+			int month= scanner.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다. \n", month, calendar.getMdaysOfMonth(month));
+		}
+		System.out.println("반복을 종료합니다.");
+		scanner.close();
 	}
 }
+
