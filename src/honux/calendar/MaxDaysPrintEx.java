35cec9337801429491d29618f139094543946266
Ariel429 <arielkim429@gmail.com>
2020-10-02 14:49:21 +0900

    method name change

--- src/honux/calendar/MaxDaysPrintEx.java
+++ src/honux/calendar/MaxDaysPrintEx.java
@@ -5,7 +5,7 @@ public class MaxDaysPrintEx {
 	// 변하지 않는 숫자값은 상수 배열로 private 접근제한자
 	private final int [] MAX_DAYS= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	// 값의 리턴이 이뤄지는 부분을 메소드로 분리해냄
-	public int mdaysOfMonth(int month) {
+	public int getMdaysOfMonth(int month) {
 		return MAX_DAYS[month+1];
 	}
 	
@@ -16,7 +16,7 @@ public class MaxDaysPrintEx {
 		
 		MaxDaysPrintEx mdaysPrint = new MaxDaysPrintEx();
 		// 객체 생성하여 메소드와 final int[]에 접근
-		System.out.printf("%d월은 %d일까지 있습니다. \n", month, mdaysPrint.mdaysOfMonth(month));
+		System.out.printf("%d월은 %d일까지 있습니다. \n", month, mdaysPrint.getMdaysOfMonth(month));
 		scanner.close();
 	}
 
